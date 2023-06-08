38013523d9722630b12d1f2bf4f4c708083743a5
rrodriguez <rrodriguez@na-at.com.mx>
2023-06-08 10:41:26 -0600

    Version 1.0.0 uploaded.

---
+++ FrameworkExample.xcframework/ios-arm64_x86_64-simulator/FrameworkExample.framework/Headers/FrameworkExample.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkExample.h
+//  FrameworkExample
+//
+//  Created by rrodriguez on 05/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkExample.
+FOUNDATION_EXPORT double FrameworkExampleVersionNumber;
+
+//! Project version string for FrameworkExample.
+FOUNDATION_EXPORT const unsigned char FrameworkExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkExample/PublicHeader.h>
+
+
